5bce7b8f3e18ec5bbe9306fd82bf462a6a5caa0d
KissLevente1 <113094877+KissLevente1@users.noreply.github.com>
2024-11-26 09:53:34 +0100

    Add project files.

---
+++ Kiss Levente Varosok/Program.cs
@@ -0,0 +1,35 @@
+﻿using Kiss_Levente_Varosok;
+
+List<Varos> varosok = [];
+
+StreamReader sr = new StreamReader(@"..\..\..\SRC\varosok.csv");
+sr.ReadLine();
+while (!sr.EndOfStream)
+{
+    varosok.Add(new Varos(sr.ReadLine()));
+}
+sr.Close();
+Console.WriteLine($"Kollekció hossza: {varosok.Count}");
+
+var osszFoKina = varosok.Where(v => v.OrszagNeve == "Kína").Sum(v => v.Nepesseg);
+Console.WriteLine($"\n{osszFoKina} millió fő él összesen a kínai nagyvárosokban.");
+
+var indiaAtlag = varosok.Where(v => v.OrszagNeve == "India").Average(v => v.Nepesseg);
+Console.WriteLine($"\n{indiaAtlag} fő az Indiai városok átlag lélekszáma. ");
+
+var legnepesebbVaros = varosok.OrderByDescending(v => v.Nepesseg).First();
+Console.WriteLine($"\nLegnepesebb város: {legnepesebbVaros}");
+
+var nepessegSzerintCsokkeno = varosok.OrderByDescending(v => v.Nepesseg).Where(v => v.Nepesseg > 20);
+Console.WriteLine($"\n20 millió lakos feletti városok csökkenő sorrendben:");
+foreach (var varos in nepessegSzerintCsokkeno)
+{
+    Console.WriteLine(varos);
+}
+
+var tobbVaros = varosok.GroupBy(v=>v.OrszagNeve).Where(group =>group.Count()>1).Count();
+Console.WriteLine($"\nTöbb nagyvárossal szerepel a listában: {tobbVaros} ország");
+
+var kezdoBetu = varosok.GroupBy(v => v.VarosNeve[0]).OrderByDescending(v=>v.Count()).First();
+Console.WriteLine($"\nA legtöbb nagyváros neve {kezdoBetu.Key} betűvel kezdődik.");
+
---
+++ Kiss Levente Varosok/Varos.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kiss_Levente_Varosok
+{
+    internal class Varos
+    {
+        public string VarosNeve { get; set; }
+        public string OrszagNeve { get; set; }
+        public double Nepesseg { get; set; }
+
+        public Varos(string s)
+        {
+            var v = s.Split(';');
+            VarosNeve = v[0];
+            OrszagNeve = v[1];
+            Nepesseg = double.Parse(v[2]);
+        }
+
+        public override string ToString()
+        {
+            return $"Város: {VarosNeve}  Ország: {OrszagNeve}  Nepesség: {Math.Round(Nepesseg),2} millió";
+        }
+    }
+}
